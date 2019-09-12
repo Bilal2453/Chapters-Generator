41065baa0dffd7fd8db925a932a5171d88ec2e7d
Bilal2453 <belal2453@gmail.com>
2019-09-12 19:07:35 +0300

    Exporting to a video is now supported

--- ChapterGen.lua
+++ ChapterGen.lua
@@ -1,10 +1,17 @@
---[[This script is under MIT license]]
+--[[
+	This script is under MIT license
+	For more details, please read this
+	https://github.com/Bilal2453/Chapter-Genrator/blob/master/LICENSE
+
+	TODO: "Options" or "Settings" GUI
+	TODO: Settings system and handling for the settings GUI
+]]
 script_name			= "Chapter Genrator"
-script_namespace	= "ChapterGen.lua"
+script_namespace	= "Bilal2453.Chapter-Genrator"
 script_description= "Generates chapter files from line's timeline."
 script_version		= '1.3.1'
 script_author		= "Bilal Bassam, Bilal2453@github.com"
-script_modified 	= "3 September 2019"
+script_modified	= "11 September 2019"
 
 local function newChapter(startTime, endTime, cpString, flaghidden, lang)
 	local tO = type(startTime) == 'table'
@@ -24,7 +31,7 @@ local function hTime(time)
 	if not time then return '' end
 	local hours, mins, secs, ms
 
-	hours = math.floor((time / 1000) / 3600)
+	hours	= math.floor((time / 1000) / 3600)
 	mins	= math.floor((time / 1000 % 3600) / 60)
 	secs	= math.floor((time / 1000) % 60)
 	ms		= math.floor((time % 1000))
@@ -75,14 +82,15 @@ local function XMLAssembler(startTime, endTime, cpString, flaghidden, lang)
 		</ChapterAtom>
 	]]):format(startTime, endTime, flaghidden, cpString, lang)
 end
+
 --------- Aegisub's GUI ---------
 local types = {
 	[".txt (OGG/OGM)"] = "OGG/OGM text (.txt)|.txt|All Files (.)|.",
 	[".xml (Matroska Chapters)"] = "Matroska Chapters (.xml)|.xml|All Files (.)|.",
 }
 
-function show(sel)
-	local GUI = {
+function show_export_ext(sel)
+	local dialog = {
 		label = {
 			class = "label",
 			label = "File extension: ",
@@ -104,11 +112,7 @@ function show(sel)
 		}
 	}
 
-	return aegisub.dialog.display(GUI, {"&Save", "&Cancel"}, {save = "&Save", cancel = "&Cancel"})
-end
-
-function save(lp, ln, lt)
-	return aegisub.dialog.save("Save Chapter", lp or '', ln or 'Chapter', lt or '')
+	return aegisub.dialog.display(dialog, {"&Save", "&Cancel"}, {save = "&Save", cancel = "&Cancel"})
 end
 
 function error(tx, ...)
@@ -132,12 +136,19 @@ end
 --------- Aegisub's modules ---------
 
 local sett = {}
-sett.path = aegisub.decode_path("?user/ChapterGen.setting")
+sett.path = aegisub.decode_path("?user/ChapterGen.settings")
 
-function sett.writeSett(d)
+function sett.createSettFile()
 	if not io.open(sett.path, 'r') then
 		io.open(sett.path, 'w'):close()
+		return true
 	end
+	return false
+end
+
+function sett.writeSett(d)
+	sett.createSettFile()
+
 	local ldas = io.open(sett.path, 'r'):read('*a')
 	local file = io.open(sett.path, 'w')
 
@@ -145,7 +156,6 @@ function sett.writeSett(d)
 	for i, v in ldas:gmatch('(.-)%s*=%s*(.-)\n') do
 		ldat[i] = v
 	end
-
 	for i, v in pairs(d) do
 		ldat[i] = v
 	end
@@ -158,21 +168,35 @@ function sett.writeSett(d)
 end
 
 function sett.readSett(f)
-	if not io.open(sett.path, 'r') then
-		io.open(sett.path, 'w'):close()
+	if sett.createSettFile() then
 		return {}
 	end
 
-	local s = io.open(sett.path, 'r'):read('*a')
+	local file = io.open(sett.path, 'r')
+	local s = file:read('*a')
 	local data = {}
 
 	for i, v in s:gmatch('(.-)%s*=%s*(.-)\n') do
 		data[i] = v
 	end
 
-	return data[f] or data
+	file:close() -- For some reason, Lua doesn't close it automaticly
+
+	return (f and data[f]) or (not f and data)
 end
 
+-- Remove some files, including settings file, so some updates can take effect
+if sett.readSett('lastUsedVersion') ~= script_version then
+	os.remove(aegisub.decode_path('?user/ChapterGen.setting'))
+	os.remove(aegisub.decode_path('?user/ChapterGen.config'))
+	os.remove(aegisub.decode_path('?user/config/ChapterGen.setting'))
+
+	os.remove(aegisub.decode_path('?user/ChapterGen.settings'))
+	sett.createSettFile()
+
+	sett.writeSett{lastUsedVersion = script_version}
+end
+--------------------------------
 
 local function parseInput(tx)
 	local options = {
@@ -183,6 +207,7 @@ local function parseInput(tx)
 
 	for n, v in tx:gmatch('%-(.-):%s*(.-)%-') do
 		-- Uhh, this is ugly...
+		-- TODO: Better input handling
 		if options[n] then
 			if tonumber(v) and options[n] == 'boolean' then
 				oInput[n] = tonumber(v) == 1 or false
@@ -202,7 +227,6 @@ local function parseInput(tx)
 
 	return oInput
 end
-
 
 local function OGMWriter(lines)
 	local str = ''
@@ -229,23 +253,28 @@ local function XMLWriter(lines)
 	return str.. '\t</EditionEntry>\n</Chapters>'
 end
 
--- Maps between file extensions and Writers
+-- Maps between file extensions and 'Writers' functions
 local wToS = {
 	['.txt (OGG/OGM)'] = OGMWriter,
 	['.xml (Matroska Chapters)'] = XMLWriter,
 }
+-- Maps between file extension's name used in GUI and the real ext
+local wToE = {
+	['.txt (OGG/OGM)'] = '.txt',
+	['.xml (Matroska Chapters)'] = '.xml',
+}
 
-function macro(lines)
+local function generateFile(lines, path, ext)
 	local fLines = {}
-
+	
 	local iTable
 	for _, v in ipairs(lines) do
 		if v.class == "dialogue" and v.comment
 		and v.effect:lower():find("chapter") then
 			iTable = {
-				cpString 	= v.text,
-				startTime 	= v.start_time,
-				endTime		= v.end_time,
+				cpString = v.text,
+				startTime= v.start_time,
+				endTime	= v.end_time,
 			}
 
 			for i, n in pairs(parseInput(v.effect:lower())) do
@@ -253,35 +282,124 @@ function macro(lines)
 			end
 
 			table.insert(fLines, iTable)
-
 		end
 	end
 
 	if #fLines < 1 then
-		error('No chapter lines were found !!')
+		return false, "No chapters were found, Are you sure you aren't drunk ?"
 	end
 
-	local rSett = sett.readSett()
+	local tData = wToS[ext](fLines)
 
-	local b, data = show(rSett.lastUsedExt)
-	if not b then aegisub.cancel() end
-
-	local tData = wToS[data.dropdown](fLines)
-
-	local saveFile = save(rSett.lastUsedPath, rSett.lastUsedName, types[data.dropdown])
-	if not saveFile then aegisub.cancel() end
-
-	local file = io.open(saveFile, 'wb')
-	if not file then error("Can't create the chapter file\nPlease try choose another directory") end
-
-	sett.writeSett{
-		lastUsedPath	= saveFile,
-		lastUsedName	= saveFile:gsub('.-\\(.-)%..+', '%1'),
-		lastUsedExt		= data.dropdown,
-	}
+	local file = io.open(path, 'wb')
+	if not file then return false, "Can't create the chapter file\nPlease try choosing another directory" end
 
 	file:write(tData)
 	file:close()
+
+	return tData
 end
 
-aegisub.register_macro(script_name, script_description, macro)
+-----------------------------------------------
+
+function macro_export_as(lines)
+	local rSett = sett.readSett()
+	
+	local b, data = show_export_ext(rSett.lastUsedExt)
+	if not b then aegisub.cancel() end
+	
+	local saveFile = aegisub.dialog.save("Export Chapter", rSett.lastUsedPath or '', rSett.lastUsedName or 'Chapter', types[data.dropdown] or '')
+	if not saveFile then aegisub.cancel() end
+	
+	sett.writeSett{
+		lastUsedPath	= saveFile:gsub('(.+%p).-%..+', '%1'),
+		lastUsedName	= saveFile:gsub('.+%p(.-)%..+', '%1'),
+		lastUsedExt		= data.dropdown,
+		lastUsedVersion= script_version,
+	}
+
+	local s, err = generateFile(lines, saveFile, data.dropdown)
+	if not s then error(err) end
+
+	for i, v in ipairs(lines) do
+		if v.class == "dialogue" and v.comment
+		and v.effect:lower():find("chapter") then
+			lines.delete(i)
+		end
+	end
+end
+
+function macro_export_to_video(lines)
+	-- Any ideas about this fnc are more than welcomed
+
+	local dp = aegisub.decode_path
+	local temps = {}
+	
+	-- bug?: Does '?data/automation/include/' dir exists on MacOS? Can't tell...
+	-- If not, please open an issue on github
+	local mkvmerge = (dp'?data'.. '/automation/include/mkvmerge.exe')
+	
+	-- TODO: Better check for mkvmerge existens, this is awful
+	if not io.open(mkvmerge, 'r') then error('mkvmerge.exe is missing\nPlease make sure mkvmerge.exe is in your automation/include dir then try again') end
+	
+	local setts = sett.readSett()
+
+	local button, data = show_export_ext(setts.lastUsedExt)
+	if not button then aegisub.cancel() end
+	data = data.dropdown
+	
+	-- Should i change this to io.tmpfile() ?
+	-- Or it sounds like a bad idea ? hmm...
+	local tmpChapDir = (dp'?temp'..'/tmp_chapter'..wToE[data])
+	table.insert(temps, tmpChapDir)
+
+	local videoSource = aegisub.dialog.open('Choose a video...', '', setts.lastUsedPath, 'All supported videos|*.mkv;*.mp4;*.m4v;*.ts;*.m2ts;*.mts;*.ogg;*.ogm;*.ogv;*.webm;*.webmv;*.mpv;*.mpg;*.mpeg;*.m1v;*.m2v;*.evo;*.evob;*.vob;*.rmvb;*.avi;*.mov;*.3gp;*.flac;*.flv;*;', false, true)
+	if not videoSource then aegisub.cancel() end
+
+	-- Create temporary chapter file
+	local s, e = generateFile(lines, tmpChapDir, data)
+	if not s then error(e) end
+
+	-- Will be used later in json options file
+	local v1, v2, v3 = videoSource, tmpChapDir, videoSource:gsub('(%p.-)%.(.+)', '%1_chap.%2')
+	if jit.os == "Windows" then
+		-- \\+\\ = \\ escaped backslash to be used in the json file
+		v1 = v1:gsub('/', '\\\\'):gsub('\\', '\\\\')
+		v2 = v2:gsub('/', '\\\\'):gsub('\\', '\\\\')
+		v3 = v3:gsub('/', '\\\\'):gsub('\\', '\\\\')
+	else
+		v1 = v1:gsub('/', '\\/')
+		v2 = v2:gsub('/', '\\/')
+		v3 = v3:gsub('/', '\\/')
+	end
+
+	-- Create temporary options file (so all platforms can be supported using only one commandline)
+	-- Actually it is not really needed, but just in case.
+	local jsonPath = dp'?temp/chap-flags.json'
+	table.insert(temps, jsonPath)
+	
+	-- Write options
+	local jsonFile = io.open(jsonPath, 'w')
+	jsonFile:write(('[\n"%s",\n"--compression",\n"-1:none",\n"--chapters",\n"%s",\n"-o",\n"%s"\n]')
+		:format(v1, v2, v3))
+	jsonFile:close()
+
+	local a1, a2, a3 = os.execute('"'..mkvmerge..'" @'..jsonPath)
+	if not a1 then
+		aegisub.debug.out("Error: Muxing Failed !\nPlease make sure you have required premisions and the video's extension is actually supported\n")
+		aegisub.debug.out("If you can't solve this problem, please report it on github at 'Bilal2453/Chapter-Genrator'\n")
+		aegisub.debug.out('Status: '..tostring(a2)..' | '.. 'Error Code: '..tostring(a3)..'\n\n')
+	end
+
+	-- Remove all temporary files
+	local success, err
+	for _, path in ipairs(temps) do
+		success, err = os.remove(path)
+		if not success then
+			aegisub.debug.out('Error: attempt to remove a temporary file:\n'.. err.. '\n')
+		end
+	end
+end
+
+aegisub.register_macro(script_name..'/Export/Export As', "Exports chapters as XML or TXT... file", macro_export_as)
+aegisub.register_macro(script_name..'/Export/Export To Video', "Exports Chapter file directly to a video. Note: this will copy the video and convert it to mkv", macro_export_to_video)
